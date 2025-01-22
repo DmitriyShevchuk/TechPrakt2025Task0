54b3a3c95daf984244545d79034ed9371989eb01
DmitriyShevchuk <ipz235_shdr@student.ztu.edu.ua>
2025-01-22 15:26:27 +0200

    add .gitignore & add script

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.Write("Enter first number: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+int b = int.Parse(Console.ReadLine());
+
+Console.Write($"{a} + {b} = {a + b}");
